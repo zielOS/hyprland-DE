43e353bec785f1fc58fcf4daec467437a153517b
Ahsanur Rahman <ahsanur041@gmail.com>
2023-08-09 09:02:20 -0600

    updated

--- .local/bin/gentoo_hyprland.sh
+++ .local/bin/gentoo_hyprland.sh
@@ -1,43 +1,35 @@
 #!/bin/bash
 
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
-makepkg -si && cd
-yay -S hyprland-nvidia-git
-yay -S hyprsome-git
-yay -S pyprland
-yay -S gtklock
-yay -S swhkd-git
-yay -S planify
-yay -S papirus-folders-catppuccin-git 
-yay -S catppuccin-gtk-theme-mocha 
-yay -S catppuccin-cursors-mocha 
-yay -S ckb-next 
-yay -S insync 
-yay -S picom-ftlabs-git 
-yay -S brave-bin
-yay -S aide
-yay -S snapper 
-yay -S snap-pac-grub
-yay -S fnm
-yay -S snapd
-yay -S zoom
-yay -S mullvad-vpn
-yay -S zramd
-yay -S nodejs-neovim
-yay -S eww-x11-git
- 
-sudo systemctl set-default graphical.target 
-systemctl --user enable --now wireplumber.service pipewire-pulse.socket pipewire.socket pipewire-pulse.service pipewire.service pipewire-pulse.socket pipewire.socket pipewire-pulse.service pipewire.service && sudo systemctl enable zramd ckb-next-daemon
+echo "Installing Dot files"
+cd && sudo rm -R ~/.config/
+cd && mkdir ~/.config  
 
-echo "Setup flatpak"
-flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+ln -s $HOME/.dots/hypr/ $HOME/.config/
+ln -s $HOME/.dots/hypr/eww $HOME/.config/
+ln -s $HOME/.dots/hypr/lazygit $HOME/.config/
+ln -s $HOME/.dots/hypr/mpv $HOME/.config/
+ln -s $HOME/.dots/hypr/ranger $HOME/.config/
+ln -s $HOME/.dots/hypr/zsh $HOME/.config/
+ln -s $HOME/.dots/hypr/.zshrc $HOME/
+ln -s $HOME/.dots/hypr/gammastep $HOME/.config/
+ln -s $HOME/.dots/hypr/gtklock $HOME/.config/
+ln -s $HOME/.dots/hypr/ranger $HOME/.config/
+ln -s $HOME/.dots/hypr/vifm $HOME/.config/
+ln -s $HOME/.dots/hypr/sxiv $HOME/.config/
+ln -s $HOME/.dots/electron-flags.conf $HOME/.config/
+ln -s $HOME/.dots/electron13-flags.conf $HOME/.config/
 
+echo "Setting up lunarvim"
+cd && mkdir ~/.npm-global && npm config set prefix '~/.npm-global'
+cd && LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+sudo rm -R $HOME/.config/lvim
+ln -s $HOME/.dots/hypr/lvim ~/.config/
 
 echo "Setting Up Systemd"
 sudo systemctl set-default graphical.target 
 systemctl --user enable --now wireplumber.service pipewire-pulse.socket pipewire.socket pipewire-pulse.service pipewire.service pipewire-pulse.socket pipewire.socket pipewire-pulse.service pipewire.service
 
-cd && mkdir ~/.npm-global && npm config set prefix '~/.npm-global' 
 
+curl -fsSL https://fnm.vercel.app/install | bash
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh)
+chsh -s $(which zsh)
